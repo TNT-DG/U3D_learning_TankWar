5ce2c799696cd1a33381daa824589e220f502cd0
TNT-DG <591810481@qq.com>
2018-05-28 21:07:34 +0800

    U3D_TankWar

---
+++ Tank_War/Assets/Scripts/Player.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //移动速度
+    public float moveSpeed = 3.0f;
+    public GameObject bulletPrefab;
+    private Vector3 bulletEulerAngles;
+    private float timeVal ;
+    //引用
+    private SpriteRenderer sr;
+    public Sprite[] tankSprite;  //上 右 下 左 0 8 16 24
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timeVal >= 0.4)
+        {
+            Attack();
+        }
+        else
+        {
+            timeVal += Time.deltaTime;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Tank_Move();
+        //Attack();
+    }
+
+    /// <summary>
+    /// 坦克移动方法
+    /// </summary>
+    private void Tank_Move()
+    {
+        //垂直轴输入
+        float v = Input.GetAxisRaw("Vertical");
+        transform.Translate(Vector3.up * v * moveSpeed * Time.fixedDeltaTime, Space.World);
+        if (v < 0)
+        {
+            sr.sprite = tankSprite[2];
+            bulletEulerAngles = new Vector3(0, 0, 180); 
+        }
+        else if (v > 0)
+        {
+            sr.sprite = tankSprite[0];
+            bulletEulerAngles = new Vector3(0, 0, 0);
+        }
+
+        if (v != 0)
+        {
+            return;
+        }
+
+
+        //水平轴输入
+        float h = Input.GetAxisRaw("Horizontal");
+        transform.Translate(Vector3.right * h * moveSpeed * Time.fixedDeltaTime, Space.World);
+        if (h < 0)
+        {
+            sr.sprite = tankSprite[3];
+            bulletEulerAngles = new Vector3(0, 0, 90);  
+        }
+        else if (h > 0)
+        {
+            sr.sprite = tankSprite[1];
+            bulletEulerAngles = new Vector3(0, 0, -90);
+        }
+    }
+
+    /// <summary>
+    /// 坦克攻击方法
+    /// </summary>
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //子弹产生的角度：当前坦克的角度+子弹应该旋转的角度
+            Instantiate(bulletPrefab, transform.position, Quaternion.Euler(transform.eulerAngles+bulletEulerAngles));
+            timeVal = 0;
+        }
+    }
+}
